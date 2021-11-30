32b4a644c3744f5d23eb48d34e9c75299b9634eb
Magdalena Rakoczy <mag.rakoczy@gmail.com>
2021-11-30 11:32:10 +0100

    calc exit

--- kalkulator_magda/main.go
+++ kalkulator_magda/main.go
@@ -14,25 +14,27 @@ func main() {
 		fmt.Print("lb2:")
 		fmt.Scanln(&lb2)
 
-		fmt.Print("operator: +,-,*,/")
+		fmt.Print("operator: +,-,*,/, exit")
 		fmt.Scanln(&operator)
 
 		switch operator {
 		case "+":
-			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1+lb2)
+			fmt.Printf("%f %s %f = %f\n", lb1, operator, lb2, lb1+lb2)
 
 		case "-":
-			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1-lb2)
+			fmt.Printf("%f %s %f = %f\n", lb1, operator, lb2, lb1-lb2)
 
 		case "*":
-			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1*lb2)
+			fmt.Printf("%f %s %f = %f\n", lb1, operator, lb2, lb1*lb2)
 
 		case "/":
 			if lb2 == 0 {
 				fmt.Println("You cannot divide by 0")
 			} else {
-				fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1/lb2)
+				fmt.Printf("%f %s %f = %f\n", lb1, operator, lb2, lb1/lb2)
 			}
+		case "exit":
+			return
 		default:
 			fmt.Println("There is not operator")
 		}
