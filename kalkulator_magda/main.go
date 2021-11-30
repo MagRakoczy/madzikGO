48d3ac46c8801a8f700ce17de380a154a1341281
mkruczek <kruczek86@gmail.com>
2021-11-30 11:25:41 +0100

    add for to kalkulator_magda

--- kalkulator_magda/main.go
+++ kalkulator_magda/main.go
@@ -7,46 +7,34 @@ func main() {
 	var lb1 float64
 	var lb2 float64
 	var operator string
+	for {
+		fmt.Print("lb1:")
+		fmt.Scanln(&lb1)
 
-	fmt.Print("lb1:")
-	fmt.Scanln(&lb1)
+		fmt.Print("lb2:")
+		fmt.Scanln(&lb2)
 
-	fmt.Print("lb2:")
-	fmt.Scanln(&lb2)
+		fmt.Print("operator: +,-,*,/")
+		fmt.Scanln(&operator)
 
-	fmt.Print("operator: +,-,*,/")
-	fmt.Scanln(&operator)
+		switch operator {
+		case "+":
+			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1+lb2)
 
-	for operator == "+" {
-		fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1+lb2)
-		break
+		case "-":
+			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1-lb2)
+
+		case "*":
+			fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1*lb2)
+
+		case "/":
+			if lb2 == 0 {
+				fmt.Println("You cannot divide by 0")
+			} else {
+				fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1/lb2)
+			}
+		default:
+			fmt.Println("There is not operator")
+		}
 	}
-	for operator == "-" {
-		fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1-lb2)
-	}
-	for operator == "*" {
-		fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1*lb2)
-	}
-	for operator == "/" {
-		fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1/lb2)
-	}
-	// switch operator {
-	// case "+":
-	// 	fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1+lb2)
-
-	// case "-":
-	// 	fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1-lb2)
-
-	// case "*":
-	// 	fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1*lb2)
-
-	// case "/":
-	// 	if lb2 == 0 {
-	// 		fmt.Println("You cannot divide by 0")
-	// 	} else {
-	// 		fmt.Printf("%f %s %f = %f", lb1, operator, lb2, lb1/lb2)
-	// 	}
-	// default:
-	// 	fmt.Println("There is not operator")
-	// }
 }
